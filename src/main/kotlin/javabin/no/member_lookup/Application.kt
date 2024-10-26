13003dfe5fcd6c6d7e8b39b7d0a746ef77a01661
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2024-10-26 12:36:49 +0200

    Load application config

--- src/main/kotlin/javabin/no/member_lookup/Application.kt
+++ src/main/kotlin/javabin/no/member_lookup/Application.kt
@@ -1,14 +1,11 @@
 package javabin.no.member_lookup
 
 import io.ktor.server.application.*
-import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import javabin.no.member_lookup.plugins.configureRouting
 
-fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
-}
+
+fun main(args: Array<String>) = EngineMain.main(args)
 
 fun Application.module() {
     configureRouting()
