8d7ae56342ea346261282f61b7248f457c9be5bc
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2024-10-26 11:10:55 +0200

    Add json logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 val kotlin_version: String by project
 val logback_version: String by project
+val logstash_version: String by project
+
 
 plugins {
     kotlin("jvm") version "2.0.21"
@@ -31,9 +33,15 @@ repositories {
 }
 
 dependencies {
+    // Server
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-test-host-jvm")
+
+    // Logging
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    runtimeOnly("net.logstash.logback:logstash-logback-encoder:$logstash_version")
+
+    // Testing
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
