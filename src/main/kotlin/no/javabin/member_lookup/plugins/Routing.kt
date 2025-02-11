2e37e0b0a569a26441ad68f4bc9df37526977825
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2025-02-11 19:43:30 +0100

    refactor: add a timeout block of 15 seconds inside runblocking

--- src/main/kotlin/no/javabin/member_lookup/plugins/Routing.kt
+++ src/main/kotlin/no/javabin/member_lookup/plugins/Routing.kt
@@ -5,13 +5,11 @@ import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import no.javabin.member_lookup.ticket.TicketService
 import no.javabin.member_lookup.ticket.integrations.checkin.CheckinTicketAdapter
 import no.javabin.member_lookup.ticket.integrations.checkin.CheckinTicketAdapterConfig
+import kotlin.time.Duration.Companion.seconds
 
 fun Application.configureRouting() {
     routing {
@@ -40,17 +38,19 @@ fun Routing.isMemberRoute() = route("membership") {
     val allEmails = mutableListOf<String>()
 
     runBlocking {
-        val events = ticketService.events()
+        withTimeout(15.seconds) {
+            val events = ticketService.events()
 
-        application.log.info("Found ${events.size} events, reading ticket emails for each event")
+            application.log.info("Found ${events.size} events, reading ticket emails for each event")
 
-        val ticketParticipantEmails = events.map { event ->
-            async {
-                ticketService.tickets(eventId = event.id)
-            }
-        }.awaitAll().flatten().map { ticket -> ticket.email }
+            val ticketParticipantEmails = events.map { event ->
+                async {
+                    ticketService.tickets(eventId = event.id)
+                }
+            }.awaitAll().flatten().map { ticket -> ticket.email }
 
-        allEmails.addAll(ticketParticipantEmails + javabinHeroesEmails)
+            allEmails.addAll(ticketParticipantEmails + javabinHeroesEmails)
+        }
     }
 
     get {
@@ -61,8 +61,6 @@ fun Routing.isMemberRoute() = route("membership") {
                 HttpStatusCode.BadRequest,
                 "We need a valid email to check for membership"
             )
-
-            application.log.info("Found ${membershipEmails.size} javaBin members, now checking membership")
 
             val hasMembership = membershipEmails.find { it == email } != null
 
